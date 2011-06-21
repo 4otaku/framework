3c73f278f9161d0174916068eaa88bbdcf855e6e
4otaku <noname-nya@yandex.ru>
2011-06-21 12:54:55 +0400

    author alias by name

--- libs/meta/author.php
+++ libs/meta/author.php
@@ -2,11 +2,17 @@
 
 class Meta_Author extends Meta_Library implements Plugins
 {
-	public function get_data_by_alias($aliases) {
+	public function get_data_by_alias ($aliases) {
 		$condition = "type='author' and ".Database::array_in('alias', $aliases);
 
 		$select = array('alias','name');
 
 		return Database::get_vector('meta', $select, $condition, $aliases, false);
-	}
+	}	
+
+	public function get_alias_by_name ($name) {
+		$condition = "type='author' and name='?'";
+
+		return Database::get_field('meta', 'alias', $condition, $name);
+	}	
 }
