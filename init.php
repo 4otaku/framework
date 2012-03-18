8e7d1b6dcb300a7346be655491130db439d0e467
Nameless <admin@4otaku.ru>
2012-03-18 16:13:33 +0400

    boot_files

--- init.php
+++ init.php
@@ -6,31 +6,3 @@ include_once 'error_handler.php';
 
 mb_internal_encoding('UTF-8');
 date_default_timezone_set('Europe/Moscow');
-
-Autoload::init(LIBS);
-
-$url = explode('/', preg_replace('/\?[^\/]+$/', '', $_SERVER['REQUEST_URI']));
-array_shift($url);
-
-$class = 'Api_' . implode('_', array_map('ucfirst', $url));
-
-if (!class_exists($class)) {
-	$class = 'Api_Error';
-	$request = new Api_Request('dummy');
-} else {
-	if (!empty($_GET['f']) && ctype_alpha($_GET['f'])) {
-		$request_type = $_GET['f'];
-		unset($_GET['f']);
-		$request = new Api_Request($request_type);
-	} else {
-		$request = new Api_Request();
-	}
-}
-
-$worker = new $class($request);
-
-if (!($worker instanceOf Api_Abstract)) {
-	$worker = new Api_Error($request);
-}
-
-echo $worker->process_request()->send_headers()->get_response();
