dec01b1e42a293a69860f5a36965e94ddbf9a22e
Nameless <admin@4otaku.ru>
2013-08-05 09:41:45 +0400

    modules to framework

---
+++ libs/module/ajax/json_error.php
@@ -0,0 +1,9 @@
+<?php
+
+class Module_Ajax_Json_Error extends Module_Ajax_Json
+{
+	public function __construct(Query $query, $disabled = false) {
+		parent::__construct($query, $disabled);
+		$this->set_error(Error::INCORRECT_URL);
+	}
+}
---
+++ libs/request/wrapper.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: derinov
+ * Date: 05.08.13
+ * Time: 8:53
+ * To change this template use File | Settings | File Templates.
+ */
