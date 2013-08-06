3033577d5b9078a0f4244693a8f5e85299e501e9
Nameless <admin@4otaku.ru>
2013-08-06 11:55:19 +0400

    refactoring

---
+++ libs/module/ajax/api.php
@@ -0,0 +1,14 @@
+<?php
+
+abstract class Module_Ajax_Api extends Module_Ajax_Json
+{
+	public function recieve_data($data) {
+		$this->set_success($data['success']);
+		if (!empty($data['errors'])) {
+			foreach ($data['errors'] as $error) {
+				$this->set_error($error['code'], $error['message']);
+			}
+		}
+	}
+}
+
