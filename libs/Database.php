3eb3750f6d8ec4066df4a9b12e93d4fe5767e0af
shtrih <shtrih@hitagi.ru>
2020-01-11 07:58:06 +0400

    Set connection options: log errors, set script time-zone

--- libs/Database.php
+++ libs/Database.php
@@ -29,7 +29,10 @@ class Database
 		}
 
 		$dsn = 'mysql:dbname=' . $database . ';host=' . $server;
-		$options = array(\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+		$options = array(
+			\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8, time_zone = '" . date('P') . "'",
+			\PDO::ATTR_ERRMODE => \PDO::ERRMODE_WARNING
+		);
 		$worker = new \PDO($dsn, $login, $password, $options);
 
 		$object = new DatabaseInstance($worker);
